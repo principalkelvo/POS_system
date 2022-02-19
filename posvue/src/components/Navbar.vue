166200a42e805cbfd5b1a86b0cbf9972bdfb3c2f
principalkelvo <kelvinnjoroowino@gmail.com>
2022-02-19 22:52:52 +0300

    creating navbar

---
+++ posvue/src/components/Navbar.vue
@@ -0,0 +1,94 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <div id="navbar-item" class="mt-3">
+        <span class="icon">
+          <i class="bx bx-menu"></i>
+        </span>
+      </div>
+      <!--search button-->
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <form method="get" action="/search">
+            <div class="field has-addons">
+              <div class="control">
+                <input
+                  type="text"
+                  class="input"
+                  placeholder="What are you looking for?"
+                  name="query"
+                />
+              </div>
+              <div class="control">
+                <button class="button">
+                  <span class="icon">
+                    <i class="fas fa-search"></i>
+                  </span>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <a
+        class="navbar-burger burger has-text-white"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbar-menu"
+        @click="showMobileMenu = !showMobileMenu"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div
+      class="navbar-menu"
+      id="navbar-menu"
+      v-bind:class="{ 'is-active': showMobileMenu }"
+    >
+      <div class="navbar-end">
+        <router-link to="/home" class="navbar-item">HOME</router-link>
+        <router-link to="/about" class="navbar-item">ABOUT US</router-link>
+        <router-link to="/contact" class="navbar-item">CONTACT</router-link>
+        <router-link to="/booking" class="navbar-item">BOOKING</router-link>
+
+        <a
+          class="navbar-item has-divider is-desktop-icon-only"
+          title="Dark mode"
+          @click="darkModeToggle"
+        >
+          <b-icon :icon="darkModeToggleIcon" custom-size="default" />
+          <span>Dark mode</span>
+        </a>
+        <div class="buttons">
+          <router-link to="/log-in" class="button is-light">Log In</router-link>
+          <router-link to="#" class="button is-light"
+            ><span class="icon"
+              ><i class="fa fa-shopping-cart" aria-hidden="true"></i></span
+            ><span>cart</span>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style>
+nav.navbar {
+  background: linear-gradient(
+    90.65deg,
+    #8a33de 21.43%,
+    rgba(147, 98, 247, 0.71) 112.7%
+  );
+  left: 260px;
+  width: calc(100% - 260px);
+}
+</style>
