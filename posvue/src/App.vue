1e090e2d10b69937e26a1dd72e9070b04def12ec
principalkelvo <kelvinnjoroowino@gmail.com>
2022-02-19 22:53:13 +0300

    main navbar entrance

--- posvue/src/App.vue
+++ posvue/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
     <Asidebar />
+    <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
 import Asidebar from "@/components/Asidebar.vue";
 export default {
   name: "App",
   components: {
-      Asidebar
+    Navbar,
+    Asidebar,
   },
 };
 </script>
 
 <style lang="scss">
-@import "../node_modules/bulma";
+@import "@/../node_modules/bulma";
+@import "../node_modules/boxicons/css/boxicons.css";
 </style>
