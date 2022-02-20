2645de02841a3e25a8791e3a3c10a68893fa3601
principalkelvo <kelvinnjoroowino@gmail.com>
2022-02-20 22:39:16 +0300

    added section

--- posvue/src/App.vue
+++ posvue/src/App.vue
@@ -2,12 +2,19 @@
   <div>
     <Asidebar />
     <Navbar />
+    <section class="section">
+      <router-view />
+    </section>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
 import Asidebar from "@/components/Asidebar.vue";
+
+
+
+
 export default {
   name: "App",
   components: {
@@ -20,4 +27,33 @@ export default {
 <style lang="scss">
 @import "@/../node_modules/bulma";
 @import "../node_modules/boxicons/css/boxicons.css";
+
+nav.navbar {
+  background: linear-gradient(
+    90.65deg,
+    #8a33de 21.43%,
+    rgba(147, 98, 247, 0.71) 112.7%
+  );
+  left: 260px;
+  width: calc(100% - 260px);
+  padding: 0 10px;
+  transition: all 0.5s ease;
+}
+
+.sidebar.close ~ .navbar {
+  left: 78px;
+  width: calc(100% - 78px);
+}
+
+section.section {
+  position: relative;
+  left: 260px;
+  width: calc(100% - 260px);
+  padding: 0 10px;
+  transition: all 0.5s ease;
+}
+.sidebar.close ~ .section {
+  left: 78px;
+  width: calc(100% - 78px);
+}
 </style>
