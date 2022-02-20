d32bf0a9b6ba7ebb7ae73d8d4fce15f6a1e4d640
principalkelvo <kelvinnjoroowino@gmail.com>
2022-02-20 22:39:39 +0300

    added event listener for arrows dropdown

--- posvue/src/main.js
+++ posvue/src/main.js
@@ -7,3 +7,11 @@ import 'boxicons'
 
 axios.defaults.baseURL= 'http://127.0.0.1:8000'
 createApp(App).use(store).use(router,axios).mount('#app')
+
+let arrow = document.querySelectorAll('.arrow');
+    for (var i = 0; i < arrow.length; i++) {
+      arrow[i].addEventListener('click', (e) => {
+        let arrowParent = e.target.parentElement.parentElement;
+        arrowParent.classList.toggle("showMenu")
+      })
+    }
