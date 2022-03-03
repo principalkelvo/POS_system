8651366e940baafdbe158b82a479c08c4a5dacef
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-03 22:45:13 +0300

    workin on barcharts

---
+++ posvue/src/components/BarChart.vue
@@ -0,0 +1,27 @@
+<script>
+import { Bar } from 'vue-chartjs'
+export default {
+    extends: Bar,
+    data(){
+        return{
+            chartData: {
+                labels: ["2019","2020","2021","2022","2023","2024"],
+                datasets:[{
+                    label: 'Bar Chart',
+                    borderWidth:1,
+                    data: [12,19,3,8,4,12,13,1,6,5,9,11,7]
+
+                }]
+            }
+
+        }
+    },
+    mounted() {
+        this.renderChart(this.chartData)
+    },
+}
+</script>
+
+<style>
+
+</style>
