c8b7b0f14a709f2ff434938ad654bc9b647e3ce7
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-03 22:46:23 +0300

    beautify

--- posvue/src/main.js
+++ posvue/src/main.js
@@ -5,13 +5,13 @@ import store from './store'
 import axios from 'axios'
 import 'boxicons'
 
-axios.defaults.baseURL= 'http://127.0.0.1:8000'
-createApp(App).use(store).use(router,axios).mount('#app')
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+createApp(App).use(store).use(router, axios).mount('#app')
 
 let arrow = document.querySelectorAll('.arrow');
-    for (var i = 0; i < arrow.length; i++) {
-      arrow[i].addEventListener('click', (e) => {
+for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener('click', (e) => {
         let arrowParent = e.target.parentElement.parentElement;
         arrowParent.classList.toggle("showMenu")
-      })
-    }
+    })
+}
