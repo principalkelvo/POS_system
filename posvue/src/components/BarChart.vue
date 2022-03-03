1d2d3206f26df57766a6a3686d4c0d05e5e5c885
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-03 23:34:25 +0300

    barchart working

--- posvue/src/components/BarChart.vue
+++ posvue/src/components/BarChart.vue
@@ -1,7 +1,12 @@
 <script>
 import { Bar } from 'vue-chartjs'
+import { h as createElement } from 'vue'
+
 export default {
+  name: 'BarChart',
+
     extends: Bar,
+
     data(){
         return{
             chartData: {
@@ -9,7 +14,7 @@ export default {
                 datasets:[{
                     label: 'Bar Chart',
                     borderWidth:1,
-                    data: [12,19,3,8,4,12,13,1,6,5,9,11,7]
+                    data: [12,19,3,8,4,16]
 
                 }]
             }
@@ -19,6 +24,22 @@ export default {
     mounted() {
         this.renderChart(this.chartData)
     },
+    render: function () {
+    return createElement(
+      'div', {
+        style: this.styles,
+        class: this.cssClasses
+      },
+      [
+        createElement(
+          'canvas', {
+            id: this.chartId,
+            ref: 'canvas'
+          }
+        )
+      ]
+    )
+  },
 }
 </script>
 
