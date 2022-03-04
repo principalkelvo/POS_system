13634887e5c1789a4e8b5f6c59e75e62047d6440
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-04 11:28:23 +0300

    links to router

--- posvue/src/router/index.js
+++ posvue/src/router/index.js
@@ -1,25 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Category from '../views/Category.vue'
+import Product from '../views/Product.vue'
+import Inventory from '../views/Inventory.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/AboutView.vue')
+    },
+    {
+        path: '/category',
+        name: 'Category',
+        component: Category
+    },
+    {
+        path: '/product',
+        name: 'Product',
+        component: Product
+    },
+    {
+        path: '/inventory',
+        name: 'Inventory',
+        component: Inventory
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+        next({ name: 'Login', query: { to: to.path } });
+    } else {
+        next()
+    }
 })
 
 export default router
