f577ca118f07adc23af684bf0024f6324ce425d8
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-05 19:48:51 +0300

    created tables for sold, returned, and transferred items

---
+++ posvue/src/components/tables/SoldProducts.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mt-5">
+          <h2
+            class="title has-text-weight-bold has-text-black is-size-4"
+          >
+            Sold Products
+          </h2>
+          <!-- <div class="column has-background-white box"></div> -->
+
+          <table class="table is-fullwidth box">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Category</th>
+                <th>Sold Quantity</th>
+                <th>Stock Status</th>
+                <th>Date Returned</th>
+                <th>Total Sell</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                </tr>
+            </tbody>
+          </table>
+          
+        </div>
+</template>
+
+<script>
+export default {
+  name:'SoldProducts'
+};
+</script>
+
+<style></style>
---
+++ posvue/src/components/tables/TransferredProducts.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mt-5">
+          <h2
+            class="title has-text-weight-bold has-text-black is-size-4"
+          >
+            Transferred Products
+          </h2>
+          <!-- <div class="column has-background-white box"></div> -->
+
+          <table class="table is-fullwidth box">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Date</th>
+                <th>From</th>
+                <th>To</th>
+                <th>Registered by</th> 
+                <th>Stock</th>
+                <th>Type</th>
+                <th>Reason</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                </tr>
+            </tbody>
+          </table>
+          
+        </div>
+</template>
+
+<script>
+export default {
+    name:'TransferredProducts'
+};
+</script>
+
+<style></style>
