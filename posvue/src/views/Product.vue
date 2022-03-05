ce523d396b5b1ef43f43160d8012092bff585157
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-05 11:00:47 +0300

    produck management

---
+++ posvue/src/views/Product.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+          <h2 class="title has-text-weight-bold has-text-black is-size-4 ">Your Stock Portfolio</h2>
+        <div class="columns is-fullwidth has-background-white box">
+              
+
+          <div class="column m-2 box has-background-faded-pink">
+            <h2 class="is-size-6  has-text-weight-bold has-text-black is-size-4">
+              Items Ordered
+            </h2>
+
+            <img
+              class="is-pulled-right"
+              src="../assets/arrows/arrows.svg"
+              alt=""
+            />
+          </div>
+          <div class="column m-2 box has-background-faded-pink">
+              <h2 class="is-size-6 has-text-weight-bold has-text-black is-size-4 ">
+              Items Received
+            </h2>
+            <img
+              class="is-pulled-right"
+              src="../assets/arrows/arrows2.svg"
+              alt=""
+            />
+          </div>
+          <div class="column m-2 box has-background-faded-pink">
+              <h2 class="is-size-6 has-text-weight-bold has-text-black is-size-4">
+              Items Out of stock
+            </h2>
+
+            <img
+              class="is-pulled-right"
+              src="../assets/arrows/arrows3.svg"
+              alt=""
+            />
+          </div>
+          <div class="column m-2 box has-background-faded-pink">
+              <h2 class="is-size-6 has-text-weight-bold has-text-black is-size-4">
+              Items Sold
+            </h2>
+
+            <img
+              class="is-pulled-right"
+              src="../assets/arrows/arrows4.svg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Product",
+};
+</script>
+
+<style scoped>
+
+</style>
