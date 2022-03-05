daa6a1e5a98b43217c0effb290630e5186ec0b14
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-05 19:47:59 +0300

    productsbox

--- posvue/src/views/Product.vue
+++ posvue/src/views/Product.vue
@@ -2,65 +2,82 @@
   <div>
     <section class="hero">
       <div class="hero-body">
-          <h2 class="title has-text-weight-bold has-text-black is-size-4 ">Producs</h2>
-        <div class="columns is-fullwidth has-background-white box">
-              
+        <div class="">
+          <h2 class="title has-text-weight-bold has-text-black is-size-4">
+            Products
+          </h2>
+          <div class="columns is-fullwidth has-background-white box">
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4"
+              >
+                Add Product
+              </h2>
 
-          <div class="column m-2 box has-background-faded-pink">
-            <h2 class="is-size-6  has-text-weight-bold has-text-black is-size-4">
-              Add Product
-            </h2>
+              <img
+                class="is-pulled-right image"
+                src="../assets/arrows/arrows.svg"
+                alt=""
+              />
+            </div>
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4"
+              >
+                Transferred Products
+              </h2>
+              <img
+                class="is-pulled-right image"
+                src="../assets/arrows/arrows2.svg"
+                alt=""
+              />
+            </div>
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4"
+              >
+                Returned Products
+              </h2>
 
-            <img
-              class="is-pulled-right"
-              src="../assets/arrows/arrows.svg"
-              alt=""
-            />
-          </div>
-          <div class="column m-2 box has-background-faded-pink">
-              <h2 class="is-size-6 has-text-weight-bold has-text-black is-size-4 ">
-              Transfer Products
-            </h2>
-            <img
-              class="is-pulled-right"
-              src="../assets/arrows/arrows2.svg"
-              alt=""
-            />
-          </div>
-          <div class="column m-2 box has-background-faded-pink">
-              <h2 class="is-size-6 has-text-weight-bold has-text-black is-size-4">
-              Returned Products
-            </h2>
+              <img
+                class="is-pulled-right image"
+                src="../assets/arrows/arrows3.svg"
+                alt=""
+              />
+            </div>
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4"
+              >
+                Sold Products
+              </h2>
 
-            <img
-              class="is-pulled-right"
-              src="../assets/arrows/arrows3.svg"
-              alt=""
-            />
+              <img
+                class="is-pulled-right image"
+                src="../assets/arrows/arrows4.svg"
+                alt=""
+              />
+            </div>
           </div>
-          <div class="column m-2 box has-background-faded-pink">
-              <h2 class="is-size-6 has-text-weight-bold has-text-black is-size-4">
-              Items Sold
-            </h2>
+          <ReturnedProducts/>
 
-            <img
-              class="is-pulled-right"
-              src="../assets/arrows/arrows4.svg"
-              alt=""
-            />
-          </div>
+           
         </div>
+
+       
+
       </div>
     </section>
   </div>
 </template>
-
 <script>
+import ReturnedProducts from "../components/tables/ReturnedProducts.vue";
 export default {
   name: "Product",
+  components: {
+    ReturnedProducts,
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
