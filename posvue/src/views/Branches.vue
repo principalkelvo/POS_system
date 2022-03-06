d01924897489304d282f4e68514d729dbc9f4f64
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-07 02:01:51 +0300

    Branches page

---
+++ posvue/src/views/Branches.vue
@@ -0,0 +1,171 @@
+<template>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <h2 class="title has-text-weight-bold has-text-black is-size-4">
+          Branches
+        </h2>
+        <div class="columns is-multiline has-background-white box is-centered">
+          <div
+            class="column m-3 box has-background-white is-2 is-webkit-center"
+          >
+            <figure class="image is-flex is-justify-content-center p-2">
+              <img src="../assets/logo.png" alt="" class="" />
+            </figure>
+            <h2 class="is-size-6 has-text-black is-size-4">Branch A</h2>
+            <h3 class="is-size-6 has-text-centered items_ordered">
+              2,465 items
+            </h3>
+            <button
+              class="button has-text-white has-text-weight-bold is-size-7 is-pink m-1"
+            >
+              Watch
+            </button>
+          </div>
+          <div
+            class="column m-3 box has-background-white is-2 is-webkit-center"
+          >
+            <figure class="image is-flex is-justify-content-center p-2">
+              <img src="../assets/logo.png" alt="" class="" />
+            </figure>
+            <h2 class="is-size-6 has-text-black is-size-4">Branch A</h2>
+            <h3 class="is-size-6 has-text-centered items_ordered">
+              2,465 items
+            </h3>
+            <button
+              class="button has-text-white has-text-weight-bold is-size-7 is-pink m-1"
+            >
+              Watch
+            </button>
+          </div>
+          <div
+            class="column m-3 box has-background-white is-2 is-webkit-center"
+          >
+            <figure class="image is-flex is-justify-content-center p-2">
+              <img src="../assets/logo.png" alt="" class="" />
+            </figure>
+            <h2 class="is-size-6 has-text-black is-size-4">Branch A</h2>
+            <h3 class="is-size-6 has-text-centered items_ordered">
+              2,465 items
+            </h3>
+            <button
+              class="button has-text-white has-text-weight-bold is-size-7 is-pink m-1"
+            >
+              Watch
+            </button>
+          </div>
+          <div
+            class="column m-3 box has-background-white is-2 is-webkit-center"
+          >
+            <figure class="image is-flex is-justify-content-center p-2">
+              <img src="../assets/logo.png" alt="" class="" />
+            </figure>
+            <h2 class="is-size-6 has-text-black is-size-4">Branch A</h2>
+            <h3 class="is-size-6 has-text-centered items_ordered">
+              2,465 items
+            </h3>
+            <button
+              class="button has-text-white has-text-weight-bold is-size-7 is-pink m-1"
+            >
+              Watch
+            </button>
+          </div>
+          <div
+            class="column m-3 box has-background-white is-2 is-webkit-center"
+          >
+            <figure class="image is-flex is-justify-content-center p-2">
+              <img src="../assets/logo.png" alt="" class="" />
+            </figure>
+            <h2 class="is-size-6 has-text-black is-size-4">Branch A</h2>
+            <h3 class="is-size-6 has-text-centered items_ordered">
+              2,465 items
+            </h3>
+            <button
+              class="button has-text-white has-text-weight-bold is-size-7 is-pink m-1"
+            >
+              Watch
+            </button>
+          </div>
+         <div
+            class="column m-3 box has-background-white is-2 is-webkit-center"
+          >
+            <figure class="image is-flex is-justify-content-center p-2">
+              <img src="../assets/logo.png" alt="" class="" />
+            </figure>
+            <h2 class="is-size-6 has-text-black is-size-4">Branch A</h2>
+            <h3 class="is-size-6 has-text-centered items_ordered">
+              2,465 items
+            </h3>
+            <button
+              class="button has-text-white has-text-weight-bold is-size-7 is-pink m-1"
+            >
+              Watch
+            </button>
+          </div>
+          <div
+            class="column m-3 box has-background-white is-2 is-webkit-center"
+          >
+            <figure class="image is-flex is-justify-content-center p-2">
+              <img src="../assets/logo.png" alt="" class="" />
+            </figure>
+            <h2 class="is-size-6 has-text-black is-size-4">Branch A</h2>
+            <h3 class="is-size-6 has-text-centered items_ordered">
+              2,465 items
+            </h3>
+            <button
+              class="button has-text-white has-text-weight-bold is-size-7 is-pink m-1"
+            >
+              Watch
+            </button>
+          </div>
+         <div
+            class="column m-3 box has-background-white is-2 is-webkit-center"
+          >
+            <figure class="image is-flex is-justify-content-center p-2">
+              <img src="../assets/logo.png" alt="" class="" />
+            </figure>
+            <h2 class="is-size-6 has-text-black is-size-4">Branch A</h2>
+            <h3 class="is-size-6 has-text-centered items_ordered">
+              2,465 items
+            </h3>
+            <button
+              class="button has-text-white has-text-weight-bold is-size-7 is-pink m-1"
+            >
+              Watch
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Branches",
+};
+</script>
+
+<style scoped>
+div.column.box {
+  border: 1px solid rgba(196, 196, 196, 0.1);
+  box-shadow: 0px 0px 1px 2px rgba(0, 0, 0, 0.06);
+}
+
+.image {
+  height: 52px;
+  width: 52px;
+}
+.items_ordered {
+  color: #958f00;
+}
+.items_received {
+  color: #169a00;
+}
+.items_out_stk {
+  color: #ff0000;
+}
+.items_sold {
+  color: #8a33de;
+}
+</style>
