a0bccbe6c97da137e6695319c6360bea82a4435f
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-10 23:34:00 +0300

    Transactions

---
+++ posvue/src/components/ViewEach/Transaction.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="mt-5">
+      <h2
+            class="title has-text-weight-bold has-text-white is-size-4"
+          >
+            Transaction No.
+          </h2>
+            <div class="columns is-multiline has-background-white box">
+              <div class="column is-4">
+                <div class="image main_image">
+                  <img src="@/assets/logo.png" alt="" class="p-2" />
+                  <!-- <i class="bx bx-pencil box"></i> -->
+                </div>
+              </div>
+              <div class="column">
+                <p> <strong>Transaction Id : </strong> 00233  </p>
+                <p> <strong>Product Purchased : </strong>product A, productB,</p>
+                <p> <strong>Transaction Date : </strong> 2/01/2023  </p>
+                <p> <strong>Customer Name : </strong>john Smith</p>
+                <p> <strong>Customer contact : </strong> 079993932  </p>
+                <p> <strong>Products Quantity : </strong> 2 items  </p>
+                <p> <strong>Transaction Status : </strong> Paid  </p>
+              </div>
+
+                <div class="column ">
+                <p> <strong>Shipping : </strong> Fargo express  </p>
+                <p> <strong>Payment method : </strong> M-pesa </p>
+                <p> <strong>VAT id : </strong> M-pesa </p>
+                <p> <strong>Location : </strong> Nai </p>
+              </div>
+
+
+                <div class="column ">
+                <p> <strong>Supplier : </strong> Loris name  </p>
+                <p> <strong>Phone Number : </strong> 07999999 </p>
+                <p> <strong>Location : </strong> Nai </p>
+              </div>
+              
+            </div>
+          </div>
+</template>
+
+<script>
+export default {
+    name:'Transaction'
+
+}
+</script>
+
+<style scoped>
+.main_image {
+  width: 10em;
+  height: 10em;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 10px;
+  padding: 1em;
+  background: #c4c4c4;
+
+}
+
+ img {
+  height: 100%;
+  width: 100%;
+  border-radius: 10px;
+  /* object-fit: cover; */
+}
+/* .main_image i {
+  position: absolute;
+  top: 0.5em;
+  right: 0.5em;
+  border-radius: 4px;
+  padding: 0.1em;
+} */
+
+</style>
---
+++ posvue/src/components/tables/Transactions.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="mt-5">
+          <h2
+            class="title has-text-weight-bold has-text-black is-size-4"
+          >
+            Transactions
+          </h2>
+          <!-- <div class="column has-background-white box"></div> -->
+
+          <table class="table is-fullwidth box">
+            <thead>
+              <tr>
+                <th>Transaction Id</th>
+                <th>Date</th>
+                <th>Paid</th>
+                <th>Status</th>
+                <th>Date</th>
+                <th>Payment Method</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                </tr>
+            </tbody>
+          </table>
+          
+        </div>
+</template>
+
+<script>
+export default {
+    name:'Transactions'
+};
+</script>
+
+<style></style>
