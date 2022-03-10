8f9830ee0fe8a8df7d73efafff5f4b379e0f7b28
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-10 11:04:04 +0300

    customers page

---
+++ posvue/src/views/Customer.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="">
+          <h2 class="title has-text-weight-bold has-text-black is-size-4">
+            Products
+          </h2>
+          <div class="columns is-fullwidth has-background-white box">
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4"
+              >
+                Add Customer
+              </h2>
+
+              <img
+                class="is-pulled-right image"
+                src="../assets/arrows/arrows.svg"
+                alt=""
+              />
+            </div>
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4"
+              >
+                View All Customers
+              </h2>
+              <img
+                class="is-pulled-right image"
+                src="../assets/arrows/arrows2.svg"
+                alt=""
+              />
+            </div>
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4"
+              >
+                Customers Chart
+              </h2>
+
+              <img
+                class="is-pulled-right image"
+                src="../assets/arrows/arrows3.svg"
+                alt=""
+              />
+            </div>
+            <!-- <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4"
+              >
+                Sold Products
+              </h2>
+
+              <img
+                class="is-pulled-right image"
+                src="../assets/arrows/arrows4.svg"
+                alt=""
+              />
+            </div> -->
+          </div>
+
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  name: "Product",
+  components: {
+  },
+};
+</script>
+
+<style scoped></style>
