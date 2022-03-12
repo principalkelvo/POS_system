357bf4f50fef935ae523bd8a8be5e9bcae0e2b27
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-12 23:47:44 +0300

    dropdown content

--- posvue/src/components/Navbar.vue
+++ posvue/src/components/Navbar.vue
@@ -59,7 +59,7 @@
       ></router-link>
 
       <div>
-        <div class="profile-details mr-5">
+        <div class="profile-details mr-5 ">
           <img src="../assets/img/CRAZY_BAE.jpg" alt="img" />
 
           <div class="name-job">
@@ -75,10 +75,39 @@
               </span>
               <!-- </div> -->
               <div class="dropdown-menu" id="dropdown-menu" role="menu">
-                <div class="dropdown-content">
-                  <a href="" class="dropdown-item"> active </a>
-                  <a href="" class="dropdown-item"> active </a>
-                </div>
+                <ul class="dropdown-content">
+                  <li class="is-flex dropdown-item">
+                    <svg class="svg-icon mr-0 text-secondary" id="h-01-p" width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                          </svg>
+                  <a href="" class=""> My Profile </a>
+                  </li>
+                  <li class="is-flex dropdown-item">
+                    <svg class="svg-icon mr-0 text-secondary" id="h-02-p" width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
+                                          </svg>
+                  <a href="" class=""> Edit Profile</a>
+                  </li>
+                  <li class="is-flex dropdown-item">
+                    <svg class="svg-icon mr-0 text-secondary" id="h-03-p" width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
+                                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                          </svg>
+                  <a href="" class=""> Account Settings </a>
+                  </li>
+                  <li class="is-flex dropdown-item">
+                    <svg class="svg-icon mr-0 text-secondary" id="h-04-p" width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path>
+                                          </svg>
+                  <a href="" class=""> Privacy Settings</a>
+                  </li>
+                  <li class="is-flex dropdown-item">
+                    <svg class="svg-icon mr-0 text-secondary" id="h-05-p" width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
+                                          </svg>
+                  <a href="" class=""> Logout </a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
@@ -171,6 +200,34 @@ input::placeholder {
   font-family: "boxicons";
   color: rgba(0, 0, 0, 0.809);
 }
+
+
+//profile dropdown
+li>a {
+    // color: #324253;
+    padding: 0 10px;
+    display: block;
+    white-space: nowrap;
+}
+a {
+    // color: #3378ff;
+    text-decoration: none;
+    background-color: transparent;
+}
+
+//dropdown
+.dropdown-menu .dropdown-item:hover {
+    color: #3378ff;
+    background: rgba(51,120,255,.1);
+}
+
+.dropdown-item:hover {
+    border-radius: 5px;
+}
+.dropdown-menu {
+  min-width: 9rem;
+}
+
 .navbar .profile-details {
   width: 100%;
   display: flex;
@@ -204,9 +261,7 @@ input::placeholder {
   font-size: 13px;
 }
 
-.dropdown-menu {
-  min-width: 9rem;
-}
+
 .navbar-item i.bx.bx-bell {
   position: "relative";
   width: 40px;
