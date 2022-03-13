7378b52ca5ce6ed94e0a4a988cddc674b8dc87b2
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-13 10:07:07 +0300

    darkmode mutation

--- posvue/src/store/index.js
+++ posvue/src/store/index.js
@@ -1,14 +1,25 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        /* Dark mode */
+        isDarkModeActive: false
+    },
+    getters: {},
+    mutations: {
+        /* Dark Mode */
+        darkModeToggle(state, payload = null) {
+            const htmlClassName = 'is-dark-mode-active'
+
+            state.isDarkModeActive = !state.isDarkModeActive
+
+            if (state.isDarkModeActive) {
+                document.documentElement.classList.add(htmlClassName)
+            } else {
+                document.documentElement.classList.remove(htmlClassName)
+            }
+        }
+    },
+    actions: {},
+    modules: {}
 })
