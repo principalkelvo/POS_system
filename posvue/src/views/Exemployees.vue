de02e73f95076be0ce699dbec936af7ce5ab02fb
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-14 12:00:01 +0300

    leave exemployees and employees pages

---
+++ posvue/src/views/Exemployees.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="">
+          <h2 class="title has-text-weight-bold has-text-black is-size-4">
+            Termination
+          </h2>
+          <div class="columns is-fullwidth has-background-white box">
+           
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ posvue/src/views/Leave.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="">
+          <h2 class="title has-text-weight-bold has-text-black is-size-4">
+            Leave
+          </h2>
+          <div class="columns is-fullwidth has-background-white box">
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4 has-text-centered"
+              >
+                Sick leave
+              </h2>
+            </div>
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4 has-text-centered"
+              >
+                Annual Leave
+              </h2>
+            </div>
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4 has-text-centered"
+              >
+                Unplanned Leave
+              </h2>
+
+            </div>
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4 has-text-centered"
+              >
+                Planned Leave
+              </h2>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
