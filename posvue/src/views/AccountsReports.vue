bd030bcc41a0bfdde28710520fe4f17a85a99b06
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-14 11:56:59 +0300

    reports for accounts,branches, customers,hr, Staffs

---
+++ posvue/src/views/AccountsReports.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="">
+          <h2 class="title has-text-weight-bold has-text-black is-size-4">
+            Accounts Reports
+            <!-- all reports dashboard -->
+          </h2>
+          <div class="columns is-fullwidth has-background-white box">
+           All Accounts table
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ posvue/src/views/StaffsReports.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="">
+          <h2 class="title has-text-weight-bold has-text-black is-size-4">
+            Staffs Reports
+            <!-- all reports dashboard -->
+          </h2>
+          <div class="columns is-fullwidth has-background-white box">
+           All Staffs Reports table
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
