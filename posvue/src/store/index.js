9468d4b2e27b2831b706aeb5f7e1f3bff62abbe5
principalkelvo <kelvinnjoroowino@gmail.com>
2022-03-14 12:37:26 +0300

    configure vuex store state for authentication

--- posvue/src/store/index.js
+++ posvue/src/store/index.js
@@ -3,7 +3,12 @@ import { createStore } from 'vuex'
 export default createStore({
     state: {
         /* Dark mode */
-        isDarkModeActive: false
+        isDarkModeActive: false,
+
+        // authentication 
+        isLoading: false,
+        isAuthenticated: false,
+        token: ''
     },
     getters: {},
     mutations: {
@@ -18,7 +23,36 @@ export default createStore({
             } else {
                 document.documentElement.classList.remove(htmlClassName)
             }
+        },
+
+        // to know if we had logged in
+        initializeStore(state) {
+            // if yes
+            if (localStorage.getItem('token')) {
+                state.token = localStorage.getItem('token')
+                state.isAuthenticated = true
+            } //if not logged in
+            else {
+                state.token = ''
+                state.isAuthenticated = false
+            }
+        },
+
+        // change of loading status
+        setIsLoading(state, status) {
+            state.isLoading = status
+        },
+        // function to set token
+        setToken(state, token) {
+            state.token = token
+            state.isAuthenticated = true
+        },
+        // function to signout
+        removeToken(state) {
+            state.token = ''
+            state.isAuthenticated = false
         }
+
     },
     actions: {},
     modules: {}
