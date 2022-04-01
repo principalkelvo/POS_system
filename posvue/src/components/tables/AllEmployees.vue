ab81580b3f25bb2277c91323879822786055b578
principalkelvo <kelvinnjoroowino@gmail.com>
2022-04-02 01:46:21 +0300

    created 2 tables

--- posvue/src/components/tables/AllEmployees.vue
+++ posvue/src/components/tables/AllEmployees.vue
@@ -1,68 +1,144 @@
 <template>
   <div class="mt-5">
-          <h2
-            class="title has-text-weight-bold has-text-black is-size-4"
-          >
-            Employees
-          </h2>
-          <!-- <div class="column has-background-white box"></div> -->
+    <h2 class="title has-text-weight-bold has-text-black is-size-4">
+      Employees
+    </h2>
+    <div class="column is-12 is-fullwidth">
+      <table class="table is-fullwidth">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Employee</th>
+            <th>ID (reg no.)</th>
+            <th>Name</th>
+            <th>Contacts</th>
+            <th>Position</th>
+            <th>Branch</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="row in rows" v-bind:key="row.id">
+            <td>{{ row.id }}</td>
 
-          <table class="table is-fullwidth box">
-            <thead>
-              <tr>
-                <th>Employee</th>
-                <th>ID (reg no.)</th>
-                <th>Name</th>
-                <th>Contacts</th>
-                <th>Position</th>
-                <th>Branch</th>
-                <th>Status</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="employee in employees" v-bind:key="employee.id">
-                <td>{{employee.get_thumbnail}}</td>
-                <td>{{employee.reg_no}}</td>
-                <td>{{employee.name}}</td>
-                <td>{{employee.email}}</td>
-                <td>{{employee.position}}</td>
-                <td>{{employee.branch}}</td>
-                <td>{{employee.status}}</td>
-                <td></td>
-                </tr>
-            </tbody>
-          </table>
-          
-        </div>
+            <td class="product-tb has-text-left">
+              <div class="image mr-2">
+                <img :src="row.get_image" class="" />
+              </div>
+            </td>
+            <td>{{ row.reg_no }}</td>
+            <td>{{ row.name }}</td>
+            <td>{{ row.email }}</td>
+            <td>{{ row.position }}</td>
+            <td>{{ row.branch }}</td>
+            <td>{{ row.status }}</td>
+            <td>{{ row.created_by }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <h1>All Posts</h1>
+
+    <vue-good-table
+      :columns="columns"
+      :rows="rows"
+      :search-options="{
+        enabled: true,
+      }"
+      :pagination-options="{
+        enabled: true,
+        perPage: 5,
+      }"
+      styleClass="vgt-table condensed"
+    >
+      <template slot="table-row" slot-scope="props">
+        <span v-if="props.column.field == 'status'">
+          age: {{ props.row.status }}
+        </span>
+        <span v-else>
+          {{ props.formattedRow[props.column.field] }}
+        </span>
+      </template>
+
+      <template slot="table-column" slot-scope="props">
+        <span v-if="props.column.label == 'Name'">
+          <i class="fa fa-address-book"></i> {{ props.column.label }}
+        </span>
+        <span v-else>
+          {{ props.column.label }}
+        </span>
+      </template>
+    </vue-good-table>
+  </div>
 </template>
 
 <script>
+import { VueGoodTable } from "vue-good-table-next";
 import axios from "axios";
-export default {
-    name:'Employees',
-    data(){
-      return{
-        employees: []
-      }
-    },
-    mounted(){
-      this.getEmployees()
-    },
-    methods: {
-      async getEmployees(){
-        await axios
-          .get('api/v1/employees/')
-          .then(response =>{
-            this.employees = response.data
-          })
-          .catch(error=>{
-            console.log(error)
-          })
-      }
-    }
 
+export default {
+  name: "Employees",
+  data() {
+    return {
+      columns: [
+        { label: "ID", field: "id" },
+        { label: "Employee", field: "get_image", type: "image" },
+        { label: "Registration Number", field: "reg_no" },
+        { label: "Name", field: "name" },
+        { label: "E-mail", field: "email" },
+        { label: "Position", field: "position" },
+        { label: "Location", field: "location" },
+        { label: "status", field: "status" },
+      ],
+      rows: [],
+    };
+  },
+  components: {
+    VueGoodTable,
+  },
+  mounted() {
+    this.getEmployees();
+  },
+  methods: {
+    // rowStyleClass(row){
+    //   if(row.image){
+    //   const image= new Image;
+    //   image.src= row.image
+    //   row.image=== image
+    //   return row.image
+    //   }
+    //   else{console.log("Imaaage")}
+    // },
+    async getEmployees() {
+      await axios
+        .get("/api/v1/employees/")
+        .then((response) => {
+          this.rows = response.data;
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.product-tb {
+  // display: flex;
+  /* align-items: center;
+        justify-content: center; */
+
+  img {
+    height: 42px;
+    min-width: 54.55px;
+    object-fit: cover;
+    border-radius: 10px;
+    padding: 0.05em;
+    background: #c4c4c4;
+  }
+}
+</style>
